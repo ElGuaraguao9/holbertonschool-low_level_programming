46a00e118614515d4b6cacd3b33aaa369151068f
Emanuel Degro <emanuel.degro@gmail.com>
2024-06-17 21:44:32 -0400

    update file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
- *Print a message
- */
-int main (void)
+*Print a message
+*/
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
