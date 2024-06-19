38dc73f59783102e1528caf21a603fc1529459bc
Emanuel Degro <emanuel.degro@gmail.com>
2024-06-19 11:52:36 -0400

    update file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,25 +4,23 @@
  *
  * Return: Always 0.
  */
-int main()
+int main(void)
 {
-    // Get the character to be written
-    char a = '_';
-    char b = 'p';
-    char c = 'u';
-    char d = 't';
-    char e = 'c';
-    char x = 'h';
-    char y = 'a';
-    char z = 'r';
-    // Write the Character to stdout
-    putchar(a);
-    putchar(b);
-    putchar(c);
-    putchar(d);
-    putchar(e);
-    putchar(x);
-    putchar(y);
-    putchar(z);
-    return (0);
+char a = '_';
+char b = 'p';
+char c = 'u';
+char d = 't';
+char e = 'c';
+char x = 'h';
+char y = 'a';
+char z = 'r';
+putchar(a);
+putchar(b);
+putchar(c);
+putchar(d);
+putchar(e);
+putchar(x);
+putchar(y);
+putchar(z);
+return (0);
 }
