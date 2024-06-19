f990b5572eae2064c346269e0410bd1fae608a1a
Emanuel <emanuel.degro@gmail.com>
2024-06-18 22:07:29 -0400

    add file

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
