15d39121c9fc246a18475196d9d6a9a82434bf4c
Emanuel <emanuel.degro@gmail.com>
2024-06-19 16:24:05 -0400

    update file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-char str[] = "_putchar";  
+char str[] = "_putchar";
 for (int i = 0; str[i] != '\0'; i++)
 {
 putchar(str[i]);
 }
 putchar('\n');
-return 0;
+return (0);
 }
