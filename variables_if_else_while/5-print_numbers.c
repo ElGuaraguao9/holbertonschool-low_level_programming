51cc2e6dd353930f539f0cbe3392d8a6a37b32c2
Emanuel <emanuel.degro@gmail.com>
2024-06-18 22:25:44 -0400

    add file

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digit 1 to 10
+ * Return: Always 0.
+ */
+int main(void)
+{
+int numb;
+for (numb = 0; numb < 10; numb++)
+printf("%d", numb);
+printf("\n");
+return (0);
+}
