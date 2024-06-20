a42c7a273e15503f0d71479c7b2b64a585355db0
Emanuel Degro <emanuel.degro@gmail.com>
2024-06-20 11:18:26 -0400

    update file

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _isupper - checks
- * 
+ *
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-return((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
