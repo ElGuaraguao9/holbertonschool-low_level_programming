4a3fc82f718498e3eb23cd2c60b25c17e5f0eff1
Emanuel Degro <emanuel.degro@gmail.com>
2024-06-20 21:33:35 -0400

    update file

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -6,10 +6,15 @@
  * @c: charater
  * Return: 1 yes 0 no
  */
+
 int _isdigit(int c)
 {
-if (c >= '0' && c <= '9')
-return (1);
-else
-return (0);
+    if (c >= '0' && c <= '9')
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
 }
