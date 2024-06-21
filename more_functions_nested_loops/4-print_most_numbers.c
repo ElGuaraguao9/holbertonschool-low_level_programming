dfb3dd398c511737eb8c90167b311292ff3ed2fd
Emanuel Degro <emanuel.degro@gmail.com>
2024-06-20 22:05:27 -0400

    update file

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_most_numbers - entry point
- *
- * Description: Prints the numbers with _putchar
- *
- * Return: void
- */
+* print_most_numbers - entry point
+*
+* Description: Prints the numbers with _putchar
+*
+* Return: void
+*/
 
 void print_most_numbers(void)
 {
